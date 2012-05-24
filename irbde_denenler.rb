af5261cf02dda3de7d913ff7884ef5f2e5f66daa
volkantufekci <volkan.tufekci@crenno.com>
2012-05-24 17:01:00 +0300

    irbde_denenler is a scratch class, will be removed soon. For now, it is cleaned

--- irbde_denenler.rb
+++ irbde_denenler.rb
@@ -1,65 +1,76 @@
 require 'rubygems'
 require 'neography'
 
+
 class IrbdeDenenler
 
-  @neo = Neography::Rest.new({:protocol => 'http://',
-                              :server => 'ec2-107-22-214-211.compute-1.amazonaws.com',
-                              :port => 7475,
-                              :directory => '',  # use '/<my directory>' or leave out for default
-                              :authentication => '', # 'basic', 'digest' or leave out for default
-                              :username => '', #leave out for default
-                              :password => '',  #leave out for default
-                              :log_file => 'neography.log',
-                              :log_enabled => false,
-                              :max_threads => 20})
-#@neo.get_root
+  attr_accessor :neo
 
-#print @neo.get_node_properties(1, ["title"])
+  def initialize
+    @neo = Neography::Rest.new({:protocol => 'http://',
+                                :server => 'ec2-107-22-214-211.compute-1.amazonaws.com',
+                                :port => 7475,
+                                :directory => '',  # use '/<my directory>' or leave out for default
+                                :authentication => '', # 'basic', 'digest' or leave out for default
+                                :username => '', #leave out for default
+                                :password => '',  #leave out for default
+                                :log_file => 'neography.log',
+                                :log_enabled => false,
+                                :max_threads => 20})
+  end
 
-  node1 = Neography::Node.load(@neo,1)
+  def gremlin_examples
+    #gremlin = @neo.execute_script("g.v(2).out('Link').title")
 
-  puts node1.title
+    #timeout aliyor
+    #gremlin = @neo.execute_script("g.v(2).outE.inV.loop(2){it.loops < 3}")
 
-  puts "node1.relationships.count: #{@neo.get_node_relationships(node1).count}"
+    #@neo.execute_script("g.v(2).out.out.id").length
 
-  puts "node1.relationships.first: ", @neo.get_node_relationships(node1).first
+    #7475'te breadth-first icin yaratilmis graphta denendi
+    #loop icinde node tekrari yapmadan, ara asamalardaki node'lari da basar
+    # result = @neo.execute_script("x=[]; g.v(30).out.except(x).aggregate(x).loop(3){it.loops < 3}{true}.name")
 
-  puts "node1 out: #{@neo.get_node_relationships(node1, "out")}"
+    h=[]
+    for i in 0..7 do ; a = @neo.execute_script("g.V[#{i}].out.id"); h[i]=a end
+    h.each do |key|
+      puts key
+    end
 
-  puts "node1.first['end']: ", @neo.get_node_relationships(node1).first["end"]
+    #puts @neo.execute_script("g.V[0].id")
+    #puts @neo.execute_script("g.v(30).id")
+  end
 
-  puts "g.v(3): ", @neo.execute_script("g.v(3)")
+  def neography_style
+    @neo.get_root
 
+    print @neo.get_node_properties(1, ["title"])
 
+    node1 = Neography::Node.load(@neo,1)
 
-  node3 = Neography::Node.load(@neo, 3)
-  outs = node3.outgoing
-  puts "node3.outs.size: #{outs.size}"
-  outs.each {|n| puts n.title}
-  node2=outs.first
+    puts node1.title
 
-# CYPHER
-  node1Hash = @neo.execute_query("start n=node(1) return n")
-  puts "Node data: #{ node1Hash["data"][0][0]["data"] }"
-  puts "keys: #{node1Hash["data"][0][0].keys}"
+    puts "node1.relationships.count: #{@neo.get_node_relationships(node1).count}"
 
-#GREMLIN
-  gremlin = @neo.execute_script("g.v(2).out('Link').title")
+    puts "node1.relationships.first: ", @neo.get_node_relationships(node1).first
 
-  #timeout aliyor
-  #gremlin = @neo.execute_script("g.v(2).outE.inV.loop(2){it.loops < 3}")
+    puts "node1 out: #{@neo.get_node_relationships(node1, "out")}"
 
-  @neo.execute_script("g.v(2).out.out.id").length
-  
-  #7475'te breadth-first icin yaratilmis graphta denendi
-  #loop icinde node tekrari yapmadan, ara asamalardaki node'lari da basar
-  # result = @neo.execute_script("x=[]; g.v(30).out.except(x).aggregate(x).loop(3){it.loops < 3}{true}.name")
+    puts "node1.first['end']: ", @neo.get_node_relationships(node1).first["end"]
 
-  for i in 1..7 do a=[]; a << @neo.execute_script("g.V[#{i}].out.id"); h["#{i}"]=a end
+    puts "g.v(3): ", @neo.execute_script("g.v(3)")
 
-  @neo.execute_script("g.V[0].id")
-  @neo.execute_script("g.v(30).id")
+    node3 = Neography::Node.load(@neo, 3)
+    outs = node3.outgoing
+    puts "node3.outs.size: #{outs.size}"
+    outs.each {|n| puts n.title}
+    node2=outs.first
+  end
 
+  def cypher_examples
+      node1Hash = @neo.execute_query("start n=node(1) return n")
+      puts "Node data: #{ node1Hash["data"][0][0]["data"] }"
+      puts "keys: #{node1Hash["data"][0][0].keys}"
+  end
 
 end
