dbe1008baea2d7021212f82305b062554f9967e1
volkantufekci <volkan.tufekci@crenno.com>
2012-05-30 14:58:44 +0300

    migrating incoming relations with creation of shadow nodes for non existing nodes in the target partition is implemented

--- partition_controller.rb
+++ partition_controller.rb
@@ -16,10 +16,6 @@ module Tez
         @log = Logger.new(STDOUT)
       end
 
-      def process_nodes_after_shadowing
-
-      end
-
       def del_rels_to_shadows_for_node (node)
         #noinspection RubyResolve
         # Collect relations to shadow node
@@ -30,12 +26,6 @@ module Tez
 
       def has_any_relation? (node)
         node.rels.size > 0
-      end
-
-      def delete_node (node)
-        node.del
-
-        # TODO Redisi Guncelle
       end
 
       def shadow_nodes (nodes)
@@ -104,98 +94,125 @@ module Tez
         Neography::Relationship.create(:knows, e, f)
       end
 
+      def migrate_node_to_partition(old_real_node, port)
+        target_partition = neo4j_instances[port]
+
+        # redis'ten target partitionda global_id'li node var miya bak
+        #noinspection RubyResolve
+        partitions_have_the_node = RedisConnector.partitions_have_the_node(old_real_node.global_id)
+
+        if partitions_have_the_node.empty?
+          @log.error("every node should at least have a partition")
+        elsif partitions_have_the_node.index(port.to_s)
+          # There is shadow node, copy properties of real node to this shadow node
+          migrate_properties_of_node(old_real_node, target_partition, false)
+          RedisConnector.add_to_partition_list_for_node(old_real_node.global_id, port)
+        else
+          # There is no shadow node in target_part, so create new real node
+          create_real_node_in_partition(old_real_node.marshal_dump, target_partition)
+        end
+
+      end
+
+      def migrate_properties_of_node(old_real_node, target_partition, will_be_shadow)
+        # Should be moved to a class extends Neography::Node
+        shadow_node_hash = get_indexed_node_from_partition(old_real_node.global_id, target_partition)
+        shadow_node_id = shadow_node_hash["self"].split('/').last
+
+        @log.info("There is shadow_node with id=#{shadow_node_id}")
+        @log.info("set node's properties to shadow node in the target partition")
+
+        properties_from_old = old_real_node.marshal_dump
+        properties_from_old[:shadow] = will_be_shadow
+        target_partition.set_node_properties(shadow_node_hash, properties_from_old)
+      end
+
       def create_real_node_in_partition(properties, partition)
         #if properties include global_id that means this is a migrate process
-        unless properties[:global_id]
+        if properties[:global_id]
+          @log.info("Node with global_id: #{properties[:global_id]} will be migrated to partition: #{partition.port}")
+        else
           global_id = RedisConnector.new_global_id
           properties[:global_id] = global_id
           RedisConnector.create_partition_list_for_node(global_id, partition.port)
-          @log.info("new global id created: #{global_id}")
+          #@log.info("new global id created: #{global_id}")
         end
 
         new_node = Neography::Node.create(properties, partition)
         @log.info("Node(#{new_node.neo_id}) with global_id: #{properties[:global_id]} created in partition with port: #{partition.port}")
-        add_node_to_globalid_index(new_node, partition)
+        add_node_to_index_of_partition(new_node, partition)
         return new_node
       end
 
-      def migrate_node_to_partition(node, partition_port)
-        target_partition = neo4j_instances[partition_port]
-
-        # redis'ten target partitionda global_id'li node var miya bak
-        #noinspection RubyResolve
-        partitions_have_the_node = RedisConnector.partitions_have_the_node(node.global_id)
-
-        if partitions_have_the_node.empty?
-          @log.error("every node should at least have a partition")
-        elsif partitions_have_the_node.index(partition_port.to_s)
-          # There is shadow node, copy properties of real node to this shadow node
-          shadow_node = get_node_from_globalid_index(node.global_id, target_partition)
-          shadow_node_id = shadow_node["self"].split('/').last
-
-          @log.info("shadow_node_id=#{shadow_node_id}")
-          @log.info("set node's properties to shadow node in the target partition")
-
-          target_partition.set_node_properties(shadow_node, node.marshal_dump)
-
-        else
-          # There is no shadow node in target_part, so create new real node
-          create_real_node_in_partition(node.marshal_dump, target_partition)
-        end
-
-      end
-
-      def add_node_to_globalid_index(node, partition)
+      def add_node_to_index_of_partition(node, partition)
         #noinspection RubyResolve
         partition.add_node_to_index(:globalidindex, :global_id, node.global_id, node)
       end
 
-      def get_node_from_globalid_index(global_id_value, partition)
+      def get_indexed_node_from_partition(global_id_value, partition)
+        @log.info("get_indexed_node_from_partition(#{global_id_value}, #{partition.port})")
         array = partition.get_node_index(:globalidindex, :global_id, global_id_value)
-        if array.empty?
+        if array.nil?
           # node, indexe eklenmemis
+          @log.info("Partition #{partition.port} has not an index with value #{global_id_value}")
+          node = nil
         else
           #node_id = array.first["self"].split('/').last
           node = array.first
         end
+
       end
 
       def migrate_incoming_rels_of_node(node_global_id, from_partition, to_partition)
 
-        end_node_from_partition = get_node_from_globalid_index( node_global_id, from_partition )
-        end_node_to_partition   = get_node_from_globalid_index( node_global_id, to_partition )
+        source_end_node_hash = get_indexed_node_from_partition( node_global_id, from_partition )
+        target_end_node_hash = get_indexed_node_from_partition( node_global_id, to_partition )
 
         #rels_in_from_partition = from_partition.get_node_relationships(end_node_from_partition, "in")
-        end_node_from_partition = Neography::Node.load(from_partition, end_node_from_partition["self"].split('/').last)
-        rels_in_from_partition = end_node_from_partition.rels.incoming
+        source_end_node = Neography::Node.load(from_partition, source_end_node_hash["self"].split('/').last)
+        rels_in_from_partition = source_end_node.rels.incoming
             rels_in_from_partition.each { |rel|
-          migrate_incoming_rel(rel, from_partition, to_partition, end_node_to_partition)
+          migrate_incoming_rel(rel, from_partition, to_partition, target_end_node_hash)
         }
       end
 
-      def migrate_incoming_rel( rel, from_partition, to_partition, end_node )
-        start_node_from_partition = rel.start_node
-        start_node_to_partition =
-            get_node_from_globalid_index( start_node_from_partition.global_id, to_partition )
+      def migrate_incoming_rel(rel, from_partition, to_partition, end_node)
+        source_start_node = rel.start_node
+        target_start_node_hash = get_indexed_node_from_partition( source_start_node.global_id, to_partition )
 
-        #new_rel =
-        #    to_partition.create_relationship(rel["type"], start_node_to_partition, end_node)
-        new_rel =
-            to_partition.create_relationship(rel.rel_type, start_node_to_partition, end_node)
+        if target_start_node_hash.nil?
+          target_start_node_hash = create_shadow_node_hash(source_start_node, to_partition)
+        end
 
-        to_partition.set_relationship_properties(new_rel, from_partition.get_relationship_properties(rel))
+        #new_rel = partition.create_relationship(rel["type"], start_node_to_partition, end_node)
+        new_rel = to_partition.create_relationship(rel.rel_type, target_start_node_hash, end_node)
+
+        properties = from_partition.get_relationship_properties(rel)
+        to_partition.set_relationship_properties(new_rel, properties) unless properties.nil?
       end
 
-      def test_move_node_to_partition
+      def create_shadow_node_hash(node, partition)
+        new_shadow_node_hash = partition.create_unique_node(:globalidindex,
+                                                            :global_id,
+                                                             node.global_id,
+                                                             node.marshal_dump)
+        partition.set_node_properties(new_shadow_node_hash, {:shadow => true})
+
+        @log.info("Shadow node with global_id: #{node.global_id} is created ")
+        return new_shadow_node_hash
+      end
+
+      def test_migrate_node_to_partition
         n2_8474 = Neography::Node.load(@neo2, 2)
         migrate_node_to_partition(n2_8474, 7474)
 
-        n1_8474 = Neography::Node.load(@neo2, 1)
-        migrate_node_to_partition(n1_8474, 7474)
+        #n1_8474 = Neography::Node.load(@neo2, 1)
+        #migrate_node_to_partition(n1_8474, 7474)
       end
 
+      #noinspection RubyInstanceMethodNamingConvention
       def test_migrate_incoming_rels_of_node
-        migrate_incoming_rels_of_node( 23, @neo2, @neo1)
+        migrate_incoming_rels_of_node( 2, @neo2, @neo1)
       end
 
     end
@@ -204,6 +221,6 @@ end
 
 pc = Tez::PartitionController.new
 #pc.preload_neo4j(pc.neo2)
-#pc.test_move_node_to_partition
+pc.test_migrate_node_to_partition
 
 pc.test_migrate_incoming_rels_of_node
