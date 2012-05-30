7c2a1da648de41ddfa15a892dfa1286e2e8f5db3
volkantufekci <volkan.tufekci@crenno.com>
2012-05-30 14:55:26 +0300

    Logging support is added to rediscontroller

--- redis_connector.rb
+++ redis_connector.rb
@@ -5,6 +5,7 @@ module RedisModul
   class RedisConnector
 
     @redis = Redis.new
+    @log = Logger.new(STDOUT)
 
     def self.new_global_id
       @redis.incr :global_neo4j_id
@@ -20,6 +21,11 @@ module RedisModul
 
     def self.create_partition_list_for_node(node_global_id, port)
       @redis.lpush node_global_id, port
+      end
+
+    def self.add_to_partition_list_for_node(node_global_id, port)
+      result = self.create_partition_list_for_node(node_global_id, port)
+      log.info("add_to_partition_list_for_node( #{node_global_id}, #{port}) => #{result}")
     end
 
     def self.remove_all
