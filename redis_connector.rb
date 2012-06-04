b291b4b6d95c522a32cb37012820e473f283a5fb
volkantufekci <volkan.tufekci@crenno.com>
2012-06-04 11:57:21 +0300

    log added to remove_all

--- redis_connector.rb
+++ redis_connector.rb
@@ -7,29 +7,30 @@ module RedisModul
     @redis = Redis.new
     @log = Logger.new(STDOUT)
 
-    def self.new_global_id
-      @redis.incr :global_neo4j_id
+    def self.add_to_partition_list_for_node(node_global_id, port)
+      result = self.create_partition_list_for_node(node_global_id, port)
+      log.info("add_to_partition_list_for_node( #{node_global_id}, #{port}) => #{result}")
     end
 
-    def self.reset_global_id
-      @redis.set :global_neo4j_id, "0"
+    def self.create_partition_list_for_node(node_global_id, port)
+      @redis.lpush node_global_id, port
+    end
+
+    def self.new_global_id
+      @redis.incr :global_neo4j_id
     end
 
     def self.partitions_have_the_node(node_global_id)
       @redis.lrange node_global_id, "0", "-1"
     end
 
-    def self.create_partition_list_for_node(node_global_id, port)
-      @redis.lpush node_global_id, port
-      end
-
-    def self.add_to_partition_list_for_node(node_global_id, port)
-      result = self.create_partition_list_for_node(node_global_id, port)
-      log.info("add_to_partition_list_for_node( #{node_global_id}, #{port}) => #{result}")
+    def self.reset_global_id
+      @redis.set :global_neo4j_id, "0"
     end
 
     def self.remove_all
       @redis.flushall
+      @log.info("Redis is reset with FLUSHALL. VT")
     end
 
   end
