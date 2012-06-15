e5ab09552ede08d4944b0fd37b7423ee5abdaf18
volkantufekci <volkan.tufekci@crenno.com>
2012-06-15 10:20:45 +0300

    In order to fill @gpart_index_array gid_nei_hash is passed to the constructor. build_grf_file and perform_mapping methods are wrapped in a method called partition_and_return_mapping

--- gpart_controller.rb
+++ gpart_controller.rb
@@ -2,11 +2,17 @@ require_relative "./configuration"
 
 class GpartController
 
-  def initialize
-    @gpart_index_array = []
+  def initialize(gid_nei_hash)
+    @gid_nei_hash = gid_nei_hash
+    @gpart_index_array = map_gids_to_gpart_indexes(gid_nei_hash)
   end
 
-  def perform_gpart_mapping
+  def partition_and_return_mapping
+    self.build_grf_file
+    self.perform_mapping
+  end
+
+  def perform_mapping
     #TODO gpart should be called to do the mapping
     `gpart 2 #{Configuration::GPART_GRF_PATH} #{Configuration::GPART_RESULT_PATH}`
     gpart_mapping = read_gpart_result
@@ -15,33 +21,28 @@ class GpartController
   end
 
   def read_gpart_result
-    words = IO.readlines(Configuration::GPART_RESULT_PATH)
+    lines = IO.readlines(Configuration::GPART_RESULT_PATH)
 
-    hash = Hash.new
-    words.each{ |line|
+    gpart_result_h = Hash.new
+    lines.each{ |line|
       if line.split(/\t/).length == 2     ####To skip the first line
-        hash[line.split(/\t/).first.to_i]=line.split(/\t/).last.split(/\n/).first.to_i
+        gpart_result_h[line.split(/\t/).first.to_i]=line.split(/\t/).last.split(/\n/).first.to_i
       end
     }
 
     hash_with_keys_plus1 = Hash.new
-    hash.each { |key, value| hash_with_keys_plus1[key+1]=value }  #Plus 1 as gpart starts with 0
+    gpart_result_h.each { |key, value| hash_with_keys_plus1[@gpart_index_array[key]]=value }  #Plus 1 as gpart starts with 0
     hash_with_keys_plus1
   end
 
-  def build_grf_file(array)
-    #for i in 1..7 do ; a = pc.neo1.execute_script("g.V[#{i}].both.id"); id = pc.neo1.execute_script("g.V[#{i}].id"); h[id.to_s]=a end
+  # values of a hash like {1=>[2, 3], 2=>[1, 3, 4, 5]} must be passed
+  def build_grf_file
+    nei_arrays_array = @gid_nei_hash.values
 
-    #h=[]
-    #for i in 1..7 do ; a = pc.neo1.execute_script("g.V[#{i}].both.id"); h[i]=a end
-    # h will be [nil, [2, 5], [5, 1, 4, 3, 6], [2], [2], [1, 2], [2], []]
-
-    #pc.neo1.execute_script("g.V.global_id")
-    #for i in 1..7 do ; a = pc.neo1.execute_script("g.V[#{i}].both.global_id"); gid = pc.neo1.execute_script("g.V[#{i}].global_id").first ; h[gid]=a end
     lines = ""
     relation_count = not_empty_node_count = 0
 
-    array.each do |neighbours|
+    nei_arrays_array.each do |neighbours|
       unless neighbours.nil? || neighbours.empty?
         not_empty_node_count += 1
         relation_count += neighbours.length
@@ -51,10 +52,10 @@ class GpartController
       end
     end
 
-    to_the_file = "0\n" << "#{not_empty_node_count}\t" << "#{relation_count}\n" << "0\t000" << lines
-    write_to_grf_file(to_the_file)
+    grf_file_content = "0\n" << "#{not_empty_node_count}\t" << "#{relation_count}\n" << "0\t000" << lines
+    write_to_grf_file(grf_file_content)
 
-    to_the_file
+    grf_file_content
   end
 
   private
@@ -83,4 +84,5 @@ class GpartController
   def map_gids_to_gpart_indexes(gid_nei_hash)
     @gpart_index_array = gid_nei_hash.keys
   end
+
 end
