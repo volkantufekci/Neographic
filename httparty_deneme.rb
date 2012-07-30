078394eb7a368e4b681ace081eb80679450fa76a
volkantufekci <volkan.tufekci@crenno.com>
2012-07-31 01:01:47 +0300

    Refactored long methods

--- httparty_deneme.rb
+++ httparty_deneme.rb
@@ -7,96 +7,67 @@ class HttpartyDeneme
   include HTTParty
 
   def initialize
-    @redis_dic = {:host => 'localhost', :port => 7379}
+    @redis_dic = {:host => 'localhost', :port => 7379}  #test_port: 7379
     @redis_connector = RedisModul::RedisConnector.new(@redis_dic)
+    @domain = "localhost"
   end
 
   def execute_script(script, server, port)
     options = { :body => {:script => script}.to_json , :headers => {'Content-Type' => 'application/json'} }
-    #configuration = "http://107.22.214.211:7474/db/data/ext/GremlinPlugin/graphdb/execute_script"
     configuration = "http://#{server}:#{port}/db/data/ext/GremlinPlugin/graphdb/execute_script"
 
-    #h = []
     response = HTTParty.post(configuration, options)
-    #response.each { |node_hash|
-    #  id = node_hash["self"].split('/').last
-    #  #h[id] = node_hash
-    #  h << id
-    #}
-
-    #h
-    #puts "#{index} => #{response.size}"
   end
 
-  def analyze(out_count)
-    @volkan =[]
+  def analyze(gid, out_count)
+    real_partition = find_real_partition_of_node(gid)
+    lid = find_lid_from_partition(gid, real_partition)
+    script = "x=0;g.v(#{lid}).out.loop(1){it.loops<#{out_count}}{true}.paths({h=[];h[0]=it.global_id;h[1]= it.shadow;h})"
+    result = execute_script(script, @domain, "6474").parsed_response
 
-    #script = 'x=0;g.v(145).sideEffect{x=x+1}.out.loop(2){it.loops<3}{true}.transform{[it.global_id,it.shadow,x]}'
-    script = "x=0;g.v(145).out.loop(1){ it.loops < #{out_count} }{ true }.paths({h=[];h[0]=it.global_id; h[1]= it.shadow;h})"
-    result = execute_script(script, "localhost", "6474").parsed_response
-    p "size=#{result.size}"
-    #result.each { |arraycik| p arraycik if arraycik.size == out_count}
+    recursive_result = run_again_for_shadows(out_count, result)
+  end
 
-    #result.each { |arraycik|
-    #  @volkan << arraycik if arraycik.size == out_count
-    #}
+  def differentiate_from_valid_partition(gid, filtered_results, out_count)
+    result_from_valid_partition = execute_on_valid_partition(gid, out_count)
+    filtered_results.each { |array|
+      unless result_from_valid_partition.include?(array)
+        puts "hede #{array.flatten}"
+      end
+    }
+  end
 
-    volkan_ayiklanmis = []
-    @volkan = run_again_for_shadows(out_count, result)
+  def filter_intermediate_paths(out_count, recursive_result)
+    filtered_results = []
     i = 0
-    @volkan.each { |arraycik|
+    recursive_result.each { |arraycik|
       if arraycik.size == out_count
         #puts "#{arraycik}\n"
-        volkan_ayiklanmis << arraycik
+        filtered_results << arraycik
         i = i + 1
       end
     }
-    puts "#{i}"
-
-    script3 = "x=0;g.v(272).out.loop(1){ it.loops < #{out_count} }.paths({h=[];h[0]=it.global_id; h[1]= it
-.shadow;h})"
-    result3 = execute_script(script3, "localhost", "8474").parsed_response
-    #fark = volkan_ayiklanmis - result3
-    volkan_ayiklanmis.each { |array|
-      unless result3.include?(array)
-        puts "hede #{array.flatten}"
-      end
-
-    }
-    #puts fark
+    puts "#{i} paths"
+    filtered_results
   end
 
   def run_again_for_shadows(out_count, result)
     sayko_array =[]
     result.each { |array|
       if array.size < out_count && array.last.last.eql?("true")
-        #puts "\n#{array}"
         gid = array.last.first
 
-        partition_port = @redis_connector.real_partition_of_node(gid).to_i
-        partition_real = Tez::PartitionController.connect_to_neo4j_instance("localhost", partition_port, @redis_dic)
-
+        partition_real = find_real_partition_of_node(gid)
         lid = partition_real.get_indexed_node(gid)["self"].split('/').last #local_id
 
-        new_out_count = out_count-array.size + 1
-        script2 = "g.v(#{lid}).out.loop(1){it.loops<#{new_out_count}}{true}.paths({h=[];h[0]=it.global_id; h[1]= it.shadow;h})"
-        new_result = execute_script(script2, "localhost", partition_port).parsed_response
-
-        #puts "size=#{new_result.size} new_out_count:#{new_out_count} "
-        #new_result.each { |arraycik| p arraycik if arraycik.size == new_out_count}
-        #new_result.each { |arraycik|
-        #
-        #  if arraycik.size == new_out_count
-        #    array.pop
-        #    array << arraycik
-        #    @volkan << array
-        #  end
-        #}
+        new_out_count = out_count - array.size + 1
+        script_for_real = "g.v(#{lid}).out.loop(1){it.loops<#{new_out_count}}{true}.paths({h=[];h[0]=it.global_id; h[1]= it.shadow;h})"
+        new_result = execute_script(script_for_real, @domain, partition_real.port).parsed_response
 
         array.pop
         new_sayko_array = run_again_for_shadows(new_out_count, new_result)
         new_sayko_array.each { |arraycik|
-          copy_array = array.compact
+          copy_array = array.compact  #compact is used just to create a copy of array
           copy_array = copy_array + arraycik
           sayko_array << copy_array
         }
@@ -107,11 +78,39 @@ class HttpartyDeneme
 
     sayko_array
   end
+
+  def find_real_partition_of_node(gid)
+    partition_port = @redis_connector.real_partition_of_node(gid).to_i
+    partition_real = Tez::PartitionController.connect_to_neo4j_instance(@domain, partition_port, @redis_dic)
+  end
+
+  def find_lid_from_partition(gid, real_partition)
+    lid = real_partition.get_indexed_node(gid)["self"].split('/').last
+  end
+
+  def execute_on_valid_partition(gid, out_count)
+    valid_partition = Tez::PartitionController.connect_to_neo4j_instance(@domain, 8474, @redis_dic)
+    lid = valid_partition.get_indexed_node(gid)["self"].split('/').last #local_id
+    script3 = "x=0;g.v(#{lid}).out.loop(1){it.loops<#{out_count}}{true}.paths({h=[];h[0]=it.global_id;h[1]= it.shadow;h})"
+    result_from_valid_partition = execute_script(script3, @domain, "8474").parsed_response
+  end
 end
 
-start = Time.now
 ht = HttpartyDeneme.new
-ht.analyze(8)
+out_count = 5
+#gid = 272
+gid = rand(1..1000)
+puts "random gid: #{gid}"
+
+start = Time.now
+results_from_nonpartitioned = ht.filter_intermediate_paths(out_count, ht.execute_on_valid_partition(gid, out_count))
+puts "results_from_nonpartitioned.size = #{results_from_nonpartitioned.size}"
+p Time.now - start
+
+start = Time.now
+results_from_partitioned = ht.filter_intermediate_paths(out_count, ht.analyze(gid, out_count))
+puts "results_from_partitioned.size = #{results_from_partitioned.size}"
+#ht.differentiate_from_valid_partition(gid, filtered_results, out_count)
 p Time.now - start
 
 #random_node = rand(1000)
@@ -127,3 +126,10 @@ p Time.now - start
 
 #23/7/2012 oncesi
 #script2 = "x=0;g.v(#{lid}).sideEffect{x=x+1}.out.loop(2){it.loops<2}{true}.transform{[it.global_id,it.shadow,x]}"
+
+
+#response.each { |node_hash|
+#  id = node_hash["self"].split('/').last
+#  #h[id] = node_hash
+#  h << id
+#}
