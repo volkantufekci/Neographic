604865bc62cce8acded5e00b95f149c5cf213bbf
volkantufekci <volkan.tufekci@crenno.com>
2012-08-03 10:54:35 +0300

    Adapted to new httparty_deneme.rb structure. Thread factory methods are separated into 2 methods such as test_partitioned and test_unpartitioned

--- test/httparty_deneme_test.rb
+++ test/httparty_deneme_test.rb
@@ -8,15 +8,22 @@ class HttpartyDenemeTest
     @thread_count = thread_count - 1
   end
 
-  def test_threads
+  def test_partitioned
+    start = Time.now
     @result_h = {}
 
     0.upto(@thread_count) { |i|
       Thread.new do
         ht = HttpartyDeneme.new
-        random_node = rand(100)
-        response = ht.execute_script("g.v(#{random_node}).out.out.out.out")
-        @result_h[Thread.current.inspect] = response
+        out_count = 5
+        gid = rand(1..1000)
+        puts "random gid: #{gid}"
+
+
+        results_from_partitioned = ht.filter_intermediate_paths(out_count, ht.analyze(gid, out_count))
+        puts "results_from_partitioned.size = #{results_from_partitioned.size}"
+
+        @result_h[Thread.current.inspect] = results_from_partitioned
       end
 
       puts "#{i} started"
@@ -27,6 +34,35 @@ class HttpartyDenemeTest
     puts "All threads should have been finished"
 
     puts @result_h.size
+    p Time.now - start
+  end
+
+  def test_nonpartitioned
+    start = Time.now
+    @result_h = {}
+
+    0.upto(@thread_count) { |i|
+      Thread.new do
+        ht = HttpartyDeneme.new
+        out_count = 5
+        gid = rand(1..1000)
+        puts "random gid: #{gid}"
+
+        results_from_nonpartitioned = ht.filter_intermediate_paths(out_count, ht.execute_on_valid_partition(gid, out_count))
+        puts "results_from_nonpartitioned.size = #{results_from_nonpartitioned.size}"
+
+        @result_h[Thread.current.inspect] = results_from_nonpartitioned
+      end
+
+      puts "#{i} started"
+    }
+
+    puts "Waiting all threads to finish"
+    Thread.list.each { |t| t.join unless t == Thread.main or t == Thread.current }
+    puts "All threads should have been finished"
+
+    puts @result_h.size
+    p Time.now - start
   end
 end
 
