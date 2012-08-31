f8d42c7d452d5d494dfb485b4fc732a6f04c51cb
Volkan Tufekci <volkan.tufekci@crenno.com>
2012-08-31 17:00:57 +0300

    Refactored in order to use lower memory and be more testing friendly by parametirizing methods.
    read_gpart_result had a bug of reading same gpart.mapping file twice

--- gpart_controller.rb
+++ gpart_controller.rb
@@ -2,39 +2,30 @@ require_relative "./configuration"
 
 class GpartController
 
-  def initialize(gid_nei_hash, neo4j_count)
-    @gid_nei_hash = gid_nei_hash
-    @gpart_index_array = map_gids_to_gpart_indexes(gid_nei_hash)
+  def initialize(neo4j_count={})
     @total_neo4j_count = neo4j_count
 
-    @log = Logger.new(STDOUT)
-    @log.level=Configuration::LOG_LEVEL
+    @log      = Logger.new(STDOUT)
+    @log.level= Configuration::LOG_LEVEL
   end
 
-  def partition_and_return_mapping
-    @log.info "PARTITION_AND_RETURN_MAPPING"
-    self.build_grf_file
-    self.perform_mapping
+  def partition_and_return_mapping(gid_relidnei_h)
+    @log.info("#{self.class.to_s}##{__method__.to_s} started")
+    self.build_grf_file(gid_relidnei_h)
+    self.perform_gparting
   end
 
-  def perform_mapping
+  def perform_gparting
     @log.info("#{self.class.to_s}##{__method__.to_s} started")
     neo4j_count = @total_neo4j_count
     `gpart #{neo4j_count} #{Configuration::GPART_GRF_PATH} #{Configuration::GPART_RESULT_PATH}`
     gpart_mapping = read_gpart_result
     gpart_mapping = inject_partition_ports(gpart_mapping)
-    #should return sth like {1=>1, 2=>0, 3=>1, 4=>0, 5=>0, 6=>0, 7=>1, 8=>1}
+    #should return sth like {1=>7474, 2=>6474, 3=>7474, ...}
   end
 
   def read_gpart_result
-    lines = ""
-    File.open(Configuration::GPART_RESULT_PATH, 'r') do |input|
-      while (line = input.gets)
-        lines << line
-      end
-    end
-    @log.info("\n#{lines}is read from the mapping file")
-
+    @log.info("#{self.class.to_s}##{__method__.to_s} started")
     gpart_result_h = Hash.new
     lines = IO.readlines(Configuration::GPART_RESULT_PATH)
     lines.each{ |line|
@@ -43,43 +34,35 @@ class GpartController
       end
     }
 
-    hash_with_keys_plus1 = Hash.new
-    gpart_result_h.each { |key, value| hash_with_keys_plus1[@gpart_index_array[key]]=value }  #Plus 1 as gpart starts with 0
-    hash_with_keys_plus1
+    gpart_result_h
   end
 
   # values of a hash like {1=>[2, 3], 2=>[1, 3, 4, 5]} must be passed
-  def build_grf_file
+  def build_grf_file(gid_relidnei_h)
     @log.info("#{self.class.to_s}##{__method__.to_s} started")
-    nei_arrays_array = @gid_nei_hash.values
+    relid_nei_h_array = gid_relidnei_h.values
 
     lines = ""
     relation_count = not_empty_node_count = 0
 
-    nei_arrays_array.each do |neighbours|
-      unless neighbours.nil? || neighbours.empty?
+    relid_nei_h_array.each do |relid_nei_h|
+      #unless neighbours.nil? || neighbours.empty?
+        neighbours = relid_nei_h.values
         not_empty_node_count += 1
         relation_count += neighbours.length
         line = "\n#{neighbours.length}"
-        neighbours.each { |node_id| line << "\t#{@gpart_index_array.index(node_id)}" }
+        neighbours.each { |node_id|
+          line << "\t#{node_id}"
+        }
+        #neighbours.each { |node_id| line << "\t#{@gpart_index_array.index(node_id.to_i)}" }
         lines << line
-      end
+      #end
     end
 
     grf_file_content = "0\n" << "#{not_empty_node_count}\t" << "#{relation_count}\n" << "0\t000" << lines
     write_to_grf_file(grf_file_content)
 
     grf_file_content
-  end
-
-  private
-
-  def write_to_grf_file(to_the_file)
-    gpart_input_file = File.new Configuration::GPART_GRF_PATH, "w"
-    gpart_input_file.write to_the_file
-    gpart_input_file.close
-
-    @log.info("\n#{to_the_file}\nis written to the grf file")
   end
 
   def inject_partition_ports(gpart_mapping)
@@ -100,8 +83,20 @@ class GpartController
     gpart_mapping
   end
 
-  def map_gids_to_gpart_indexes(gid_nei_hash)
-    @gpart_index_array = gid_nei_hash.keys
+  private
+
+  def write_to_grf_file(to_the_file)
+    @log.info("#{self.class.to_s}##{__method__.to_s} started")
+    gpart_input_file = File.new Configuration::GPART_GRF_PATH, "w"
+    gpart_input_file.write to_the_file
+    gpart_input_file.close
+
+    @log.debug("\n#{to_the_file}\nis written to the grf file")
   end
+
+
+  #def map_gids_to_gpart_indexes(gid_nei_hash)
+  #  @gpart_index_array = gid_nei_hash.keys
+  #end
 
 end
