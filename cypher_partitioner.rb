c0daac96876a899f30581d0d2b53d5b024a9fc2a
Volkan Tufekci <volkan.tufekci@crenno.com>
2012-11-02 20:05:08 +0200

    CypherPartitioner with methods for reading gid_partition_h file and injecting port numbers are implemented.
    Their tests are added

---
+++ cypher_partitioner.rb
@@ -0,0 +1,43 @@
+require_relative 'configuration'
+
+class CypherPartitioner
+
+  def initialize
+    @log      = Logger.new(STDOUT)
+    @log.level= Configuration::LOG_LEVEL
+  end
+
+  def return_partition_mapping
+    inject_partition_ports(read_partition_mapping)
+  end
+
+  def read_partition_mapping
+    gid_partition_h = {}
+
+    lines = IO.readlines(Configuration::GID_PARTITION_H)
+    lines.each do |line|
+      tokens = line.chomp.split(",")
+      gid       = tokens[0]
+      partition = tokens[1]
+      gid_partition_h[gid] = partition
+    end
+  end
+
+  def inject_partition_ports(mapping)
+    @log.info("#{self.class.to_s}##{__method__.to_s} started")
+    #0, 1'leri 7474 8474 gibi portlarla degistir
+    mapping.each_pair do |gid,partition|
+      case partition
+        when 0
+          mapping[gid] = 6474
+        when 1
+          mapping[gid] = 7474
+        when 2
+          mapping[gid] = 8474
+        else
+          mapping[gid] = 9474
+      end
+    end
+    mapping
+  end
+end
---
+++ test/cyper_partitioner_test.rb
@@ -0,0 +1,37 @@
+require "test/unit"
+require "../cypher_partitioner"
+
+class CypherPartitionerTest < Test::Unit::TestCase
+
+  require 'minitest/reporters'
+  MiniTest::Unit.runner = MiniTest::SuiteRunner.new
+  MiniTest::Unit.runner.reporters << MiniTest::Reporters::RubyMineReporter.new
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    @cypher_partition = CypherPartitioner.new
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  # Fake test
+  def test_read_partition_mapping
+    mapping = @cypher_partition.read_partition_mapping
+    assert(mapping.size > 0, "there is no line in the file")
+  end
+
+  def test_inject_partition_ports
+    mapping = {555 => 0, 666 => 1, 777 => 0, 888 => 3}
+    injected_mapping = @cypher_partition.inject_partition_ports(mapping)
+    assert(injected_mapping[555] == 6474, "expected: 6474, actual: #{injected_mapping[555]}")
+    assert(injected_mapping[666] == 7474, "wrong port injection")
+    assert(injected_mapping[777] == 6474, "wrong port injection")
+    assert(injected_mapping[888] == 9474, "wrong port injection")
+  end
+end
