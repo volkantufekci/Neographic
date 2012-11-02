a1a703f36a3ab8fdaf66d1cdc33e3aa271c97ae8
Volkan Tufekci <volkan.tufekci@crenno.com>
2012-11-02 12:50:49 +0200

    Running jar code is added

--- for_debug.rb
+++ for_debug.rb
@@ -2,7 +2,10 @@ require_relative './partition_controller'
 require_relative './redis_connector'
 require_relative './gpart_controller'
 
-max_node_idx        = 99999
+start = Time.now
+puts "Started at: #{start}"
+
+max_node_idx        = 9
 total_neo4j_count   = 2
 
 
@@ -18,6 +21,20 @@ gid_partition_h     = gpc.partition_and_return_mapping(gid_relidnei_h)
 pc                  = Tez::PartitionController.new
 pc.generate_csvs(gid_partition_h, gid_relidnei_h)
 
-puts "Finished at: #{Time.now}"
+puts "Time elapsed: #{Time.now - start}"
+
+#partition = 7474
+#p "~/neo4j/#{partition}/bin/neo4j stop"
+#`~/neo4j/#{partition}/bin/neo4j stop`
+#
+#p "rm -r ~/neo4j/#{partition}/data/graph.db"
+#`rm -r ~/neo4j/#{partition}/data/graph.db`
+#
+#p "java -server -Xmx2G -jar ~/batch-import-jar-with-dependencies.jar ~/neo4j/#{partition}/data/graph.db ~/partitioned_csv_dir/#{partition}/nodes.csv ~/partitioned_csv_dir/#{partition}/rels.csv"
+#`java -server -Xmx2G -jar ~/batch-import-jar-with-dependencies.jar ~/neo4j/#{partition}/data/graph.db ~/partitioned_csv_dir/#{partition}/nodes.csv ~/partitioned_csv_dir/#{partition}/rels.csv`
+#
+#p "~/neo4j/#{partition}/bin/neo4j start"
+#`~/neo4j/#{partition}/bin/neo4j start`
+
 
 #jruby -J-Xmx3000m -w for_debug.rb
