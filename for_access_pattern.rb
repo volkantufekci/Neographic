bb14bbe1c0464068246c919ad1c118e5a78aee01
Volkan Tufekci <volkan.tufekci@crenno.com>
2013-02-21 00:16:49 +0200

    read_partition method was unreachable

--- for_access_pattern.rb
+++ for_access_pattern.rb
@@ -1,20 +1,6 @@
+require_relative './redis_connector_erdos'
 require_relative './partition_controller_erdos'
 
-start = Time.now
-puts "Started at: #{start}"
-
-max_node_idx        = 1850065 #553000 #9
-
-rc                  = RedisModul::RedisConnectorErdos.new
-#rc.fill
-
-gid_relidnei_h      = rc.fetch_relations max_node_idx
-gid_partition_h     = read_partition_mapping()
-#gpc                 = GpartController.new(total_neo4j_count)
-
-Tez::PartitionControllerErdos.new.generate_csvs(gid_partition_h, gid_relidnei_h)
-
-puts "Toplam sure: #{Time.now - start}"
 
 def read_partition_mapping
   gid_partition_h = {}
@@ -28,3 +14,19 @@ def read_partition_mapping
 
   gid_partition_h
 end
+
+start = Time.now
+puts "Started at: #{start}"
+
+max_node_idx        = 1850065 #553000 #9
+
+rc                  = RedisModul::RedisConnectorErdos.new
+#rc.fill
+
+gid_relidnei_h      = rc.fetch_relations max_node_idx
+gid_partition_h     = read_partition_mapping
+#gpc                 = GpartController.new(total_neo4j_count)
+
+Tez::PartitionControllerErdos.new.generate_csvs(gid_partition_h, gid_relidnei_h)
+
+puts "Toplam sure: #{Time.now - start}"
